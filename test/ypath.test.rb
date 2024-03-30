7a1a62a3a83dca582aa9df7890beac3deb05a411
LAVN <rairairakurakulilac@hotmail.co.jp>
2024-03-30 19:48:17 +0900

    syckのypath実装およびテスト追加

---
+++ test/ypath.test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'yaml'
+require "coverage"
+require_relative "../lib/ryresolv/ypath.rb"
+
+class TestYPath < Minitest::Test
+
+  def assert_path_segments( path, segments, msg = nil )
+    Syck::YPath.each_path( path ) { |choice|
+      assert_equal( choice.segments, segments.shift, msg )
+    }
+    assert_equal( segments.length, 0, "Some segments leftover: #{ segments.inspect }" )
+  end
+
+  def test_ypath
+    assert_path_segments( "/*/((one|three)/name|place)|//place",
+      [ ["*", "one", "name"],
+        ["*", "three", "name"],
+        ["*", "place"],
+        ["/", "place"] ]
+    )
+  end
+
+end
